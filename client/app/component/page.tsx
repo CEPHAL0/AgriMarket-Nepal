f1abc1dfb37272c47b1fb37dffcafbde7d1237d6
CEPHAL0 <xaradxarma@gmail.com>
2024-04-23 22:29:25 +0545

    Added Consumable Cards

--- client/app/component/page.tsx
+++ client/app/component/page.tsx
@@ -1,4 +1,11 @@
 import ConsumableCard from "@/components/Consumables/ConsumableCard";
+import Vegetable from "@/public/Vegetables.jpg";
 export default function Page() {
-  return <ConsumableCard image="test.png" name="Radish" type="FRUIT" />;
+  return (
+    <div className="m-6 flex gap-4">
+      <ConsumableCard image={Vegetable} name="Radish" type="VEGETABLE" />
+      <ConsumableCard image={Vegetable} name="Apple" type="FRUIT" />
+      <ConsumableCard image={Vegetable} name="Milk" type="OTHER" />
+    </div>
+  );
 }
--- client/components/Consumables/ConsumableCard.tsx
+++ client/components/Consumables/ConsumableCard.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react";
 import { ConsumableTypeString } from "@/app/lib/definitions";
+import Image, { StaticImageData } from "next/image";
 export default function ConsumableCard({
   name,
   type,
@@ -7,11 +8,33 @@ export default function ConsumableCard({
 }: {
   name: string;
   type: ConsumableTypeString;
-  image: string;
+  image: StaticImageData;
 }): ReactElement {
+  const typeClassMap = {
+    FRUIT: "bg-fruit",
+    VEGETABLE: "bg-vegetable",
+    OTHER: "bg-other",
+  };
+
   return (
-    <div>
-      <div className="h-44 bg-fruit"></div>
+    <div
+      className={`h-72 w-64 rounded-xl flex flex-col shadow-md shadow-primary-gray ${typeClassMap[type]}`}
+    >
+      <div className="h-[70%] relative w-full">
+        <Image
+          src={image}
+          alt="name"
+          priority={true}
+          className="object-cover rounded-t-xl"
+          fill
+        />
+      </div>
+      <div className="flex items-center justify-between p-4 grow">
+        <p className=" font-semibold text-3xl text-primary-blue">{name}</p>
+        <span className="bg-black w-fit h-fit rounded-xl px-2 py-1 text-xs text-white">
+          {type.toUpperCase()}
+        </span>
+      </div>
     </div>
   );
 }
