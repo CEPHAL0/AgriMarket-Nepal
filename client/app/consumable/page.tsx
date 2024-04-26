fd74e592fa5b2d890edded8e5c3abfad894c7aae
CEPHAL0 <xaradxarma@gmail.com>
2024-04-26 10:41:42 +0545

    Completed Cards

--- client/app/consumable/page.tsx
+++ client/app/consumable/page.tsx
@@ -1,49 +1,19 @@
-"use client";
+import ConsumableListingCards from "@/sections/ConsumableListingCards";
 
-import ConsumableCard from "@/components/Consumables/ConsumableCard";
-import { useState, useEffect } from "react";
-import Vegetable from "@/public/Vegetables.jpg";
-import { fetchConsumables } from "./fetchConsumables";
+import { ReactElement, Suspense } from "react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+function ErrorComponent(): ReactElement {
+  return <div>Failed to get message...</div>;
+}
 
 export default function Page() {
-  const [res, setRes] = useState<any>();
-
-  useEffect(() => {
-    getConsumables()
-      .then((data) => {
-        setRes(data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }, []);
-
   return (
     <div className="px-4 py-6 flex flex-col items-center justify-center gap-8">
       <h1 className="text-4xl font-bold text-primary-green">Consumbales</h1>
-      {res ? (
-        <div className="flex flex-wrap justify-center content-start gap-10 w-full">
-          {res.map((consumable: any) => {
-            return (
-              <ConsumableCard
-                key={consumable.id}
-                image={`${API_URL}/${consumable.image_path}`}
-                name={consumable.name}
-                type={consumable.type}
-              />
-            );
-          })}
-        </div>
-      ) : (
-        "Loading..."
-      )}
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <ConsumableListingCards />
+      </Suspense>
     </div>
   );
-}
-
-export async function getConsumables() {
-  let data = await fetchConsumables();
-  return data;
 }
